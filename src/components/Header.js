8f8f7bb1cb58e93855ad0b9a419a2b15941c5665
sagalosman <sagalosman0@gmail.com>
2022-11-06 18:02:34 +0000

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+// import link
+import { Link } from 'react-router-dom';
+// import logo
+import Logo from '../assets/img/logo.svg';
+
+const Header = () => {
+  return (
+    <header className='py-6 mb-12 border-b'>
+      <div className='container mx-auto flex justify-between items-center'>
+        <Link to='/'>
+          <img src={Logo} alt='' />
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
